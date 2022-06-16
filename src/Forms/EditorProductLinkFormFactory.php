21f6deaaa9334e7850a9b518837f693ddf725928
Vince Cabrera <vince.cabrera@riobravo.co.nz>
2022-06-16 17:47:09 +1200

    Update and rename EditorPhoneLinkFormFactory.php to EditorProductLinkFormFactory.php

---
+++ src/Forms/EditorProductLinkFormFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace VinceCabrera\ProductLink\Forms;
+
+use SilverStripe\Admin\Forms\LinkFormFactory;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\DropdownField;
+
+class EditorProductLinkFormFactory extends LinkFormFactory{
+
+    protected function getFormFields($controller, $name, $context) {
+        $products = \Product::get()->filter(["InventoryStatus" => "Current"])->sort("InventoryCode")->map("ID", "InventoryCode");
+
+        $fields = FieldList::create([
+            DropdownField::create(
+                'Link',
+                'Product',
+                $products
+            ),
+            TextField::create(
+                'Description',
+                'Description (optional)'
+            )
+        ]);
+
+        if ($context['RequireLinkText']) {
+            $fields->insertAfter('Link', TextField::create('Text', 'Link text'));
+        }
+
+        return $fields;
+    }
+
+    protected function getValidator($controller, $name, $context) {
+        if ($context['RequireLinkText']) {
+            return RequiredFields::create('Text');
+        }
+
+        return null;
+    }
+}
+
