b49e69cd5d35d22b0433d1c1f3569e068ac45000
Vince Cabrera <vince.cabrera@riobravo.co.nz>
2022-06-16 17:57:39 +1200

    Update _config.php

--- _config.php
+++ _config.php
@@ -3,11 +3,11 @@ use SilverStripe\Core\Manifest\ModuleLoader;
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 
 call_user_func(function () {
-    $module = ModuleLoader::inst()->getManifest()->getModule('firebrandhq/silverstripe-phonelink');
+    $module = ModuleLoader::inst()->getManifest()->getModule('VinceCabrera/silverstripe-productlink');
 
-    // Enable insert-link to phone numbers
+    // Enable insert-link to products
     TinyMCEConfig::get('cms')
         ->enablePlugins([
-            'sslinkphone' => $module->getResource('client/dist/js/TinyMCE_sslink-phone.js'),
+            'sslinkphone' => $module->getResource('client/dist/js/TinyMCE_sslink-product.js'),
         ]);
 });
