66babc53dd570ff37649097fe44e8115b7e48a06
Vince Cabrera <vince.cabrera@riobravo.co.nz>
2022-06-21 15:01:49 +1200

    Update LeftAndMainExtension.php

--- src/Extensions/LeftAndMainExtension.php
+++ src/Extensions/LeftAndMainExtension.php
@@ -3,13 +3,19 @@
 namespace VinceCabrera\ProductLink\Extensions;
 
 use SilverStripe\Core\Extension;
+use SilverStripe\Dev\Debug;
 
 class LeftAndMainExtension extends Extension
 {
     public function updateClientConfig(&$config)
     {
-        $config['form']['EditorPhoneLink'] = [
+
+        $config['form']['EditorProductLink'] = [
             'schemaUrl' => $this->getOwner()->Link('methodSchema/Modals/EditorProductLink'),
+        ];
+
+        $config['form']['EditorPCWLink'] = [
+            'schemaUrl' => $this->getOwner()->Link('methodSchema/Modals/EditorPCWLink'),
         ];
     }
 }
