7d54a6bf607f7e5e0f41b1bcfe64c2f3620d57b2
Vince Cabrera <vince.cabrera@riobravo.co.nz>
2022-06-27 10:43:52 +1200

    Update EditorPCWLinkFormFactory.php

--- src/Forms/EditorPCWLinkFormFactory.php
+++ src/Forms/EditorPCWLinkFormFactory.php
@@ -2,22 +2,32 @@
 
 namespace VinceCabrera\ProductLink\Forms;
 
+use SilverStripe\Dev\Debug;
 use SilverStripe\Admin\Forms\LinkFormFactory;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\RequiredFields;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\DropdownField;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\View\ArrayData;
 
 class EditorPCWLinkFormFactory extends LinkFormFactory{
 
     protected function getFormFields($controller, $name, $context) {
-        $products = \Product::get()->filter(["InventoryStatus" => "Current"])->sort("InventoryCode")->map("ID", "InventoryCode");
+
+        $records = \PCWRecord::get()->filter(["Status" => "CURRENT"])->sort("TypeID, Name");
+        $pcwRecordss = new ArrayList();
+        foreach ($records as $pcwRecord){
+            $pcwRecordss->push(new ArrayData(["ID" => $pcwRecord->ID, "Name" => $pcwRecord->Type()->Name . ": " . $pcwRecord->Name]));
+        }
+
+        $pcwRecords = $pcwRecordss->sort("Name")->map("ID", "Name");
 
         $fields = FieldList::create([
             DropdownField::create(
                 'Link',
                 'PCW Record',
-                $products
+                $pcwRecords
             ),
             TextField::create(
                 'Description',
@@ -40,4 +50,3 @@ class EditorPCWLinkFormFactory extends LinkFormFactory{
         return null;
     }
 }
-
